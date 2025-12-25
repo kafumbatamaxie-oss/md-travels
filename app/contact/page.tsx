6fa4de93bbf4543b62e0b711a2b4d30ce095e422
Maxie <kafumbatamaxie@gmail.com>
2025-12-25 23:20:31 +0200

    Implement  Confirmation Page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -80,7 +80,7 @@ export default function Contact() {
               { icon: Mail, title: "Email Us on", content: "info@mdtravels.co.za", href: "mailto:info@mdtravels.co.za" },
               { icon: MapPin, title: "Visit Us at", content: "44 Wrench Street, Parow West, 7500", href: "#" },
             ].map((item, i) => (
-              <p className="border-b py-4 text-underline underline decoration-indigo-500">{item.title.toUpperCase()} : <span><a href={item.href}>{item.content}</a></span></p>
+              <p className="border-b py-4">{item.title.toUpperCase()} : <span className="text-underline underline hover:decoration-indigo-500 "><a href={item.href}>{item.content}</a></span></p>
             ))}
           </div>
 
